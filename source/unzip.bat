9e90d70f0c80890d83eba4645f7e601ba6cf6c97
vodinhhoang2006 <vodinhhoang17072006@gmail.com>
2026-02-27 16:06:21 +0700

    New changes
    I modified the unzip.bat file to match the current project's software name.

--- source/unzip.bat
+++ source/unzip.bat
@@ -1,6 +1,6 @@
 @echo off
 title "Updating is in progress, please do not close this window"
-powershell -command "Expand-Archive -Path 'vdhYoutubeDownloader-.zip' -DestinationPath '.' -Force"
-del vdhYoutubeDownloader-.zip
+powershell -command "Expand-Archive -Path 'vdh_youtube_downloader.zip' -DestinationPath '.' -Force"
+del vdh_youtube_downloader.zip
 start "" VDH_YouTube_Downloader.exe
 exit
